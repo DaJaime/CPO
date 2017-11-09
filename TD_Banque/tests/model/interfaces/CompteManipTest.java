7c1b6e7b0fcbc4aa37d73290fe6587a2125a8bdd
Damien Jaime <damjai@MacBook-Air-de-Damien.local>
2017-11-09 18:09:24 +0100

    Ajout evoSuite

--- TD_Banque/tests/model/interfaces/CompteManipTest.java
+++ TD_Banque/tests/model/interfaces/CompteManipTest.java
@@ -2,10 +2,12 @@ package model.interfaces;
 
 import model.exceptions.InsufficientBalance;
 import model.fact.CompteFactory;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.function.DoublePredicate;
+import java.util.stream.DoubleStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,6 +33,11 @@ class CompteManipTest {
         cm.setBalance(0.0d);
     }
 
+    @AfterAll
+    void tearDownAll(){
+        cm = null;
+    }
+
     @Test
     void testWithdraw() throws InsufficientBalance {
         double oldBal = cm.getBalance();
@@ -39,4 +46,25 @@ class CompteManipTest {
         assertEquals(newBal,oldBal - amount);
     }
 
+    @ParameterizedTest
+    @MethodSource("range500To1000")
+    void withdrawWithParam(double amount) throws InsufficientBalance{
+        double oldBal = cm.getBalance();
+        double newBal = cm.withdraw(amount);
+        assertEquals(newBal, oldBal - amount);
+    }
+
+    static DoubleStream range500To1000(){
+        return DoubleStream.iterate(500.0d, n -> n +100.0d).limit(6);
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeForOverdraft")
+    void withdrawWithParamExpectOverdraft(double amount){
+        assertThrows(InsufficientBalance.class, () -> cm.withdraw(amount));
+    }
+
+    static DoubleStream rangeForOverdraft(){
+        return DoubleStream.iterate(1100.0d, n -> n + 100.0d).limit(3);
+    }
 }
